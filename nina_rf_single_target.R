c58414b8713810348475767a77d809dd9ccb9e6f
Jovan Tanevski <tanevski@gmail.com>
2019-08-29 14:57:26 +0200

    parameter tuning for each model migrate to caret

--- nina_rf_single_target.R
+++ nina_rf_single_target.R
@@ -2,122 +2,102 @@ library(readr)
 library(caret)
 library(randomForest)
 library(purrr)
+library(furrr)
 library(corrplot)
 library(dplyr)
 
 set.seed(42)
+plan(multiprocess, workers=14)
 
+## Example
 datapath <- commandArgs(TRUE)[1]
+train_model(datapath)
 
-d <- read_csv(paste0(datapath, ".txt"), col_types = cols())[, -1]
-
-targets <- colnames(d)[1:14]
-features <- paste(colnames(d)[15:ncol(d)], collapse = "+")
-
-outls <- targets %>% map(function(target){
-  pulled <- d %>% pull(target) %>% hist(breaks = "FD", plot = FALSE)
-  bottom <- pulled$breaks[which.max(pulled$counts)]
-  top <- pulled$breaks[which.max(pulled$counts) + 1]
-  which(d %>% pull(target) <= top & d %>% pull(target) > bottom)
-})
+load(paste0(datapath,".RData"))
+model_stats(model)
 
 
-cts <- table(unlist(outls))
-to.remove <- as.numeric(names(which(cts > 10)))
-
-par(mfrow = c(3,5))
-targets %>% walk(function(target){
- pulled <- d %>% slice(-to.remove) %>% pull(target) %>%  hist(breaks = "FD", plot = FALSE)
- plot(pulled, main = target)
-})
-
-#filter out most frequent parameter values
-d <- d %>% slice(-to.remove)
-
-folds <- createFolds(seq(nrow(d)), k = 10)
-
-# pdf(file = paste0(datapath, "_dist.pdf"), width = 10.5, height = 8.5)
-# par(mfrow = c(3, 5))
-# seq(14) %>% walk(~ plot(density(t(d[, .x])), main = targets[.x]))
-# corrplot(cor(d[, 1:14]), type = "upper", diag = F)
-# dev.off()
-# 
-# model <- targets %>% map(~ randomForest(as.formula(paste0(.x, "~", features)),
-#   data = d, ntree = 100, importance = T
-# ))
-# 
-# save(model, file = paste0(datapath, ".RData"))
-
-
-#
-defaulteval <- folds %>% map(function(fold) {
-  print("Folding")
-  targets %>%
-    map(~ mean(t(d[-fold, .x]))) %>%
-    imap_dbl(~ sqrt(sum((.x - d[fold, .y])^2) / length(fold)))
-})
-
-defaultstats <- defaulteval %>% map_dfr(~.x)
-
-rfeval <- folds %>% map(function(fold) {
-  print("Folding")
-  predictions <- targets %>%
-    map(~ randomForest(as.formula(paste0(.x, "~", features)), data = d[-fold, ], ntree = 100)) %>%
-    map(~ predict(.x, d[fold, 15:ncol(d)]))
-  write_csv(cbind(d[fold, 1:14], predictions), path = paste0(datapath, "_predictions.csv"), append = TRUE, col_names = FALSE)
-  predictions %>% imap_dbl(~ sqrt(sum((.x - d[fold, .y])^2) / length(fold)))
-})
-
-rfstats <- rfeval %>% map_dfr(~.x)
-
-tmean <- folds %>% map_dfr(function(fold) {
-  targets %>% map_dbl(~ mean(t(d[-fold, .x])))
-})
-
-nrmse <- rfstats / tmean
-
-rrmse <- rfstats / defaultstats
-
-grmse <- rfstats %>% colMeans()
-gnrmse <- nrmse %>% colMeans()
-grrmse <- rrmse %>% colMeans()
-
-write("RMSE", file = paste0(datapath, "_perf.txt"), append = TRUE)
-write.table(t(rfstats %>% rowMeans()), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(apply(rfstats, 1, sd)), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(c(mean(grmse), sd(grmse))), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-
-write("NRMSE", file = paste0(datapath, "_perf.txt"), append = T)
-write.table(t(nrmse %>% rowMeans()), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(apply(nrmse, 1, sd)), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(c(mean(gnrmse), sd(gnrmse))), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-
-write("RRMSE", file = paste0(datapath, "_perf.txt"), append = T)
-write.table(t(rrmse %>% rowMeans()), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(apply(rrmse, 1, sd)), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
-write.table(t(c(mean(grrmse), sd(grrmse))), file = paste0(datapath, "_perf.txt"), col.names = FALSE, row.names = FALSE, append = TRUE)
+train_model <- function(datapath){
+  d <- read_csv(paste0(datapath, ".txt"), col_types = cols())[, -1]
+  
+  targets <- colnames(d)[1:14]
+  features <- paste(colnames(d)[15:ncol(d)], collapse = "+")
+  
+  
+  #trim data
+  outls <- targets %>% map(function(target){
+    pulled <- d %>% pull(target) %>% hist(breaks = "FD", plot = FALSE)
+    bottom <- pulled$breaks[which.max(pulled$counts)]
+    top <- pulled$breaks[which.max(pulled$counts) + 1]
+    which(d %>% pull(target) <= top & d %>% pull(target) > bottom)
+  })
+  
+  
+  cts <- table(unlist(outls))
+  to.remove <- as.numeric(names(which(cts > 10)))
+  
+  #plot distributions and correlation
+  pdf(file = paste0(datapath, "_dist.pdf"), width = 10.5, height = 8.5)
+  par(mfrow = c(3,5))
+  targets %>% walk(function(target){
+   pulled <- d %>% slice(-to.remove) %>% pull(target) %>%  hist(breaks = "FD", plot = FALSE)
+   plot(pulled, main = target)
+  })
+  corrplot(cor(d %>% slice(-to.remove) %>% select(targets)), type = "upper", diag = F)
+  dev.off()
+  
+  #filter out most frequent parameter values
+  d <- d %>% slice(-to.remove)
+  
+  #folds <- createFolds(seq(nrow(d)), k = 10)
+  
+  flength <- ncol(d) - length(targets)
+  #fit model parameters
+  if(flength < 200){
+    mtries <- data.frame(mtry = c(1, floor(c(0.1,0.25,0.5,1)*flength), floor(sqrt(flength))))
+    model <- targets %>% future_map(function(target){
+      train(as.formula(paste0(target, "~", features)), d, method = "rf",
+            ntree = 100, metric = "RMSE", tuneGrid = mtries, 
+            trControl = trainControl(method = "cv", number = 10, savePredictions = "final"))
+    })
+  } else {
+    #PCA
+    model <- targets %>% future_map(function(target){
+      flength <- 11
+      mtries <- data.frame(mtry = c(1, floor(c(0.1,0.25,0.5,1)*flength), floor(sqrt(flength))))
+      train(as.formula(paste0(target, "~", features)), d, method = "rf",
+            ntree = 100, metric = "RMSE", tuneGrid = mtries, 
+            trControl = trainControl(method = "cv", number = 10, savePredictions = "final", preProcOptions = list(thresh = 1)),
+            preProcess="pca")
+    })
+  }
+  
+  save(model, file = paste0(datapath, ".RData"))
+  return(model)
+}
 
 
-# imps <- model %>% map_dfc(~importance(.x)[,2])
+model_stats <- function(model){
+  model %>% map_dfr(function(m){
+    stats <- unique(m$pred$Resample) %>% map_dfr(function(fold){
+      fold.idx <- m$pred %>% filter(Resample == fold) %>% pull(rowIndex)
+      
+      rmse <- m$pred %>% group_by(Resample) %>% summarise(rmse = RMSE(pred, obs)) %>% pull(rmse)
+      
+      means <- m$trainingData %>% 
+        slice(-fold.idx) %>%
+        pull(.outcome) %>% mean
+  
+      default.stats <- (means - m$trainingData %>% slice(fold.idx) %>% pull(.outcome))^2 %>% mean %>% sqrt
+      
+      
+      tibble(rmse = rmse, nrmse = rmse/means, rrmse = rmse/default.stats)
+      
+    })
+    
+    tibble(rmse.mean = mean(stats$rmse), rmse.sd = sd(stats$rmse), 
+           nrmse.mean = mean(stats$nrmse), nrmse.sd = sd(stats$nrmse), 
+           rrmse.mean = mean(stats$rrmse), rrmse.sd = sd(stats$rrmse))
+  })
+}
 
-# imps %>% apply(2,order,decreasing=T) %>% rowMeans()
-
-# imps %>% apply(2,function(p){
-#   (p-min(p))/(max(p)-min(p))
-# })
-
-# heatmap(imps)
-
-# p <- read_csv("data2_predictions.csv", col_names = FALSE)
-#
-# seq(14) %>% walk(function(.x){
-#   pdf(file = paste0("set1_2/",targets[.x],".pdf"),width=8,height=5)
-#   #smoothScatter(t(p[,.x]),t(p[,.x+14]),xlab="True value", ylab="Predicted value",main=targets[.x], nrpoints=0, colramp=colorRampPalette(c("white","grey10")))
-#   smoothScatter(t(p[,.x]),t(p[,.x+14]),xlab="True value", ylab="Predicted value",main=targets[.x], pch='.', nrpoints=0, colramp=colorRampPalette(c("white","lightyellow", "darkseagreen1","royalblue")))
-#   #samp <- sample(nrow(p),1000)
-#   points(t(p[,.x]),t(p[,.x+14]),pch='.')
-#   #plot(t(p[,.x]),t(p[,.x+14]),xlab="True value",ylab="Predicted value",main=targets[.x],pch='.')
-#   dev.off()
-#
-# }
-# )
--- pmodel_evaluate.R
+++ pmodel_evaluate.R
@@ -1,39 +1,37 @@
-#hello kitty
+# hello kitty
 library(readr)
+library(dplyr)
 library(randomForest)
 library(purrr)
+library(caret)
 
-evaluate <- function(query,model,pca = FALSE) {
-  colnames(query) <- c(paste0("sig",seq(length(query)-14)), paste0("spk",seq(14)))
+evaluate <- function(query, model, pca = FALSE) {
+  colnames(query) <- c(paste0("sig", seq(length(query) - 14)), paste0("spk", seq(14)))
   
-  if(pca){
-    query <- as_tibble(scale(query,center = pcs$center, scale = pcs$scale) %*% pcs$rotation) %>%
-      select(1:components)
-    colnames(query) <- paste0("PC",seq(components))
+  #use the rotation vector from the first model, as all should be identical
+  if(!is.null(model[[1]]$preProcess)){
+    query <- scale(query, center = model[[1]]$preProcess$mean, scale = model[[1]]$preProcess$std) %*% 
+      model[[1]]$preProcess$rotation
+    colnames(query) <- paste0("PC",seq(model[[1]]$preProcess$numComp))
   }
+
+  t0 <- Sys.time()
+  predictions <- model %>% map(~ predict(.x$finalModel, query, predict.all = TRUE))
+  t1 <- Sys.time()
+  print(t1 - t0)
   
-  t0<-Sys.time()
-  predictions <- model %>% map(~predict(.x,query,predict.all=TRUE))
-  t1<-Sys.time()
-  print(t1-t0)
   predictions
 }
 
-load("path_to_model.RData")
-is.pca <- FALSE
+load("journal/nina_data_vsi.RData")
 
-#transpose galore 
-result <- read_csv("input_data.csv",col_types = cols(), col_names = FALSE) %>% 
-  apply(1,function(q) evaluate(as.data.frame(t(q)),model, is.pca))
+# transpose galore
+result <- read_csv("test_nina_pca.txt", col_types = cols(), col_names = FALSE) %>%
+  apply(1, function(q) evaluate(as.data.frame(t(q)), model))
 
 i <- 1
 result %>% sapply(function(r) {
-  names(r) <- c("F","Bd1","depi","mel","oxy","der","Bd","A","Be","oxy2","amp","pot","amp1","pot1")
-  write.csv(t(r %>% map_dfr(~.x$individual)),paste0("output_data_",i,".csv"))
-  i <<- i+1
+  names(r) <- c("F", "Bd1", "depi", "mel", "oxy", "der", "Bd", "A", "Be", "oxy2", "amp", "pot", "amp1", "pot1")
+  write.csv(t(r %>% map_dfr(~ .x$individual)), paste0("output_data_", i, ".csv"))
+  i <<- i + 1
 })
-
-
-
-
-
