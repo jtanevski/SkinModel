30b651ccc1dbff0a9ea2d9dfb0335ea8f795e7d7
Jovan Tanevski <tanevski@gmail.com>
2019-08-30 13:35:38 +0200

    add function for cv scatter plots per target code style changes

--- nina_rf_single_target.R
+++ nina_rf_single_target.R
@@ -7,97 +7,145 @@ library(corrplot)
 library(dplyr)
 
 set.seed(42)
-plan(multiprocess, workers=14)
+plan(multiprocess, workers = 14)
 
 ## Example
 datapath <- commandArgs(TRUE)[1]
 train_model(datapath)
 
-load(paste0(datapath,".RData"))
+load(paste0(datapath, ".RData"))
 model_stats(model)
 
 
-train_model <- function(datapath){
+train_model <- function(datapath) {
   d <- read_csv(paste0(datapath, ".txt"), col_types = cols())[, -1]
-  
+
   targets <- colnames(d)[1:14]
   features <- paste(colnames(d)[15:ncol(d)], collapse = "+")
-  
-  
-  #trim data
-  outls <- targets %>% map(function(target){
-    pulled <- d %>% pull(target) %>% hist(breaks = "FD", plot = FALSE)
+
+
+  # trim data
+  outls <- targets %>% map(function(target) {
+    pulled <- d %>%
+      pull(target) %>%
+      hist(breaks = "FD", plot = FALSE)
     bottom <- pulled$breaks[which.max(pulled$counts)]
     top <- pulled$breaks[which.max(pulled$counts) + 1]
     which(d %>% pull(target) <= top & d %>% pull(target) > bottom)
   })
-  
-  
+
+
   cts <- table(unlist(outls))
   to.remove <- as.numeric(names(which(cts > 10)))
-  
-  #plot distributions and correlation
+
+  # plot distributions and correlation
   pdf(file = paste0(datapath, "_dist.pdf"), width = 10.5, height = 8.5)
-  par(mfrow = c(3,5))
-  targets %>% walk(function(target){
-   pulled <- d %>% slice(-to.remove) %>% pull(target) %>%  hist(breaks = "FD", plot = FALSE)
-   plot(pulled, main = target)
+  par(mfrow = c(3, 5))
+  targets %>% walk(function(target) {
+    pulled <- d %>%
+      slice(-to.remove) %>%
+      pull(target) %>%
+      hist(breaks = "FD", plot = FALSE)
+    plot(pulled, main = target)
   })
   corrplot(cor(d %>% slice(-to.remove) %>% select(targets)), type = "upper", diag = F)
   dev.off()
-  
-  #filter out most frequent parameter values
+
+  # filter out most frequent parameter values
   d <- d %>% slice(-to.remove)
-  
-  #folds <- createFolds(seq(nrow(d)), k = 10)
-  
+
+  # folds <- createFolds(seq(nrow(d)), k = 10)
+
   flength <- ncol(d) - length(targets)
-  #fit model parameters
-  if(flength < 200){
-    mtries <- data.frame(mtry = c(1, floor(c(0.1,0.25,0.5,1)*flength), floor(sqrt(flength))))
-    model <- targets %>% future_map(function(target){
-      train(as.formula(paste0(target, "~", features)), d, method = "rf",
-            ntree = 100, metric = "RMSE", tuneGrid = mtries, 
-            trControl = trainControl(method = "cv", number = 10, savePredictions = "final"))
+  # fit model parameters
+  if (flength < 200) {
+    mtries <- data.frame(mtry = c(
+      1, floor(c(0.1, 0.25, 0.5, 1) * flength),
+      floor(sqrt(flength))
+    ))
+    model <- targets %>% future_map(function(target) {
+      train(as.formula(paste0(target, "~", features)), d,
+        method = "rf",
+        ntree = 100, metric = "RMSE", tuneGrid = mtries,
+        trControl = trainControl(
+          method = "cv", number = 10,
+          savePredictions = "final"
+        )
+      )
     })
   } else {
-    #PCA
-    model <- targets %>% future_map(function(target){
+    # PCA
+    model <- targets %>% future_map(function(target) {
       flength <- 11
-      mtries <- data.frame(mtry = c(1, floor(c(0.1,0.25,0.5,1)*flength), floor(sqrt(flength))))
-      train(as.formula(paste0(target, "~", features)), d, method = "rf",
-            ntree = 100, metric = "RMSE", tuneGrid = mtries, 
-            trControl = trainControl(method = "cv", number = 10, savePredictions = "final", preProcOptions = list(pcaComp = 11)),
-            preProcess="pca")
+      mtries <- data.frame(mtry = c(
+        1, floor(c(0.1, 0.25, 0.5, 1) * flength),
+        floor(sqrt(flength))
+      ))
+      train(as.formula(paste0(target, "~", features)), d,
+        method = "rf",
+        ntree = 100, metric = "RMSE", tuneGrid = mtries,
+        trControl = trainControl(
+          method = "cv", number = 10,
+          savePredictions = "final",
+          preProcOptions = list(pcaComp = 11)
+        ),
+        preProcess = "pca"
+      )
     })
   }
-  
+
   save(model, file = paste0(datapath, ".RData"))
   return(model)
 }
 
 
-model_stats <- function(model){
-  model %>% map_dfr(function(m){
-    stats <- unique(m$pred$Resample) %>% map_dfr(function(fold){
-      fold.idx <- m$pred %>% filter(Resample == fold) %>% pull(rowIndex)
-      
-      rmse <- m$pred %>% group_by(Resample) %>% summarise(rmse = RMSE(pred, obs)) %>% pull(rmse)
-      
-      means <- m$trainingData %>% 
+model_stats <- function(model) {
+  model %>% map_dfr(function(m) {
+    stats <- unique(m$pred$Resample) %>% map_dfr(function(fold) {
+      fold.idx <- m$pred %>%
+        filter(Resample == fold) %>%
+        pull(rowIndex)
+
+      rmse <- m$pred %>%
+        group_by(Resample) %>%
+        summarise(rmse = RMSE(pred, obs)) %>%
+        pull(rmse)
+
+      means <- m$trainingData %>%
         slice(-fold.idx) %>%
-        pull(.outcome) %>% mean
-  
-      default.stats <- (means - m$trainingData %>% slice(fold.idx) %>% pull(.outcome))^2 %>% mean %>% sqrt
-      
-      
-      tibble(rmse = rmse, nrmse = rmse/means, rrmse = rmse/default.stats)
-      
+        pull(.outcome) %>%
+        mean()
+
+      default.stats <- (means - m$trainingData %>% slice(fold.idx) %>% pull(.outcome))^2 %>%
+        mean() %>%
+        sqrt()
+
+
+      tibble(rmse = rmse, nrmse = rmse / means, rrmse = rmse / default.stats)
     })
-    
-    tibble(rmse.mean = mean(stats$rmse), rmse.sd = sd(stats$rmse), 
-           nrmse.mean = mean(stats$nrmse), nrmse.sd = sd(stats$nrmse), 
-           rrmse.mean = mean(stats$rrmse), rrmse.sd = sd(stats$rrmse))
+
+    tibble(
+      rmse.mean = mean(stats$rmse), rmse.sd = sd(stats$rmse),
+      nrmse.mean = mean(stats$nrmse), nrmse.sd = sd(stats$nrmse),
+      rrmse.mean = mean(stats$rrmse), rrmse.sd = sd(stats$rrmse)
+    )
   })
 }
 
+
+prediction_scatter_plots <- function(model, folder) {
+  p <- model %>%
+    map_dfc(~ tibble(!!.x$terms[[2]] := .x$pred$obs)) %>%
+    cbind(model %>% map_dfc(~ tibble(!!.x$terms[[2]] := .x$pred$pred)))
+
+  seq(14) %>% walk(function(.x) {
+    pdf(file = paste0(folder, colnames(p)[.x], ".pdf"), width = 8, height = 5)
+    smoothScatter(t(p[, .x]), t(p[, .x + 14]),
+      xlab = "True value", ylab = "Predicted value",
+      main = colnames(p)[.x], pch = ".", nrpoints = 0,
+      colramp = colorRampPalette(c("white", "lightyellow", "darkseagreen1", "royalblue"))
+    )
+    points(t(p[, .x]), t(p[, .x + 14]), pch = ".")
+    dev.off()
+  })
+}
